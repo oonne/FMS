c1e63e6120b3a343d16dfddf6a2651b64c4e85f4
JAY <JAY@oonne.com>
2020-04-22 17:59:08 +0800

    调用登录接口

--- wx/miniprogram/pages/index/index.js
+++ wx/miniprogram/pages/index/index.js
@@ -1,33 +1,53 @@
+import config from '../../config/config'
+
 Page({
   data: {
   },
-
-  h1 () {
-    wx.request({
-      url: 'https://afms.oonne.com',
-      enableHttp2: false,
-      data: {
+  onLoad() {
+    wx.showNavigationBarLoading()
+    // 必须在冷启动时在取数据预加载的内容，不然会取到上次的
+    if (wx.getBackgroundFetchData) {
+      // 数据预加载
+      wx.getBackgroundFetchData({
+        fetchType: 'pre',
+        success: res=> {
+          const data = JSON.parse(res.fetchedData)
+          this.loginSuccess(data)
+        },
+        fail: res=> {
+          this.fetchLogin()
+        },
+      })
+    } else {
+      this.fetchLogin()
+    }
+  },
+  /**
+   * 进入小程序的登录请求
+   * （开发/测试环境使用，或作为生产环境数据预加载失败的兜底）
+   * @param {String} env 当前环境
+   * @return {String} path 落地页需要跳转的页面
+   */
+  fetchLogin () {
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        wx.hideNavigationBarLoading()
+        this.loginSuccess(res.result)
       },
-      header: {
-        'content-type': 'application/json'
-      },
-      success (res) {
-        console.log(res)
+      fail: err => {
+        wx.hideNavigationBarLoading()
+        // 登录失败
+        // TODO
       }
     })
   },
-  h2 () {
-    wx.request({
-      url: 'https://afms.oonne.com',
-      enableHttp2: true,
-      data: {
-      },
-      header: {
-        'content-type': 'application/json'
-      },
-      success (res) {
-        console.log(res)
-      }
-    })
+
+  /**
+   * 登录接口调用完成的处理
+   * @param {Object} result 登录接口返回结果
+   */
+  loginSuccess (result) {
+    console.log(result)
   },
 })
