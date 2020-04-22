7f3106629564be9abbf0e51a7bc890fef99c7ba9
JAY <JAY@oonne.com>
2020-04-22 17:27:56 +0800

    数据预加载

--- wx/miniprogram/app.js
+++ wx/miniprogram/app.js
@@ -2,13 +2,15 @@ import config from 'config/config'
 
 App({
   globalData: {
-    token: 'eyJhbGciOiJIUzI1NiJ9.eyJVVUlEIjoiIiwiVFlQRSI6MywiTE9HSUNfSUQiOjEwMDIwMDAwMDAwMDAwMjU0MiwiZXhwIjoxNTkwMDY0OTg4LCJuYmYiOjE1ODc0NzI5ODh9.vyQrZhArNn-_ic63ljY-Q9qmBrlDGDPLwBF25KMmRKk',
-    coldStart: true, //是否冷启动
+    openId: '',
+    token: '',
   },
   onLaunch: function (options) {
     console.log("-------------Launch", config.version, options)
     // 初始化云函数环境
     this.cloudInit()
+    // 检查更新版本
+    this.updataVersion()
   },
   /*初始化云函数环境*/ 
   cloudInit () {
@@ -16,5 +18,23 @@ App({
       env: config.cloudEnv,
       traceUser: true,
     })
+  },
+  /*更新版本*/ 
+  updataVersion () {
+    try {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onUpdateReady(function () {
+        setTimeout(function () {
+          wx.showModal({
+            content: '新版本已经准备好，点击重启应用',
+            showCancel: false,
+            success: function (res) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate()
+            }
+          })
+        }, 2000)
+      })
+    } catch (e) {}
   },
 })
