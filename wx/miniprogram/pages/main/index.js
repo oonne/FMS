0b1eb7cc7ee3840fc741245b2cab7439af69176e
JAY <JAY@oonne.com>
2020-04-23 14:13:16 +0800

    初步规划首页逻辑

--- wx/miniprogram/pages/main/index.js
+++ wx/miniprogram/pages/main/index.js
@@ -2,9 +2,38 @@ import config from '../../config/config'
 
 Page({
   data: {
+    auth: false, //是否有家庭权限
+    //云笔记
+    cloudNotes: {
+      count: 0, //总数量
+      lastNote: {}, //最后更新的笔记
+    },
+    // 消费
+    expenses: {
+      daily: 0, //当日统计
+      monthly: 0, //当月统计
+    },
+    // 收入
+    income: {
+      monthly: 0, //当月统计
+    },
+    //记事本
+    notes: {
+      count: 0, //总数量
+      lastNote: {}, //最后更新的笔记
+    },
+    //日记
+    diary: {
+      date: '', //当前日期
+      content: '今日暂无内容', //今日日记
+    },
   },
-  onLoad() {
+  onLoad () {
     this.login()
+  },
+  onShow () {
+    this.getCloudNotes()
+    this.getDatas()
   },
   /**
    * 登录(从数据预拉取拿结果，或者直接发起请求)
@@ -27,11 +56,7 @@ Page({
       this.fetchLogin()
     }
   },
-  /**
-   * 直接发起登录请求
-   * @param {String} env 当前环境
-   * @return {String} path 落地页需要跳转的页面
-   */
+  // 直接发起登录请求
   fetchLogin () {
     console.warn('直接发起登录请求')
     wx.cloud.callFunction({
@@ -41,15 +66,43 @@ Page({
       this.loginSuccess(res.result)
     })
   },
-  /**
-   * 登录接口调用完成的处理
-   * @param {Object} result 登录接口返回结果
-   */
+  // 登录接口返回之后的逻辑
   loginSuccess (result) {
     getApp().globalData.openId = result.wxContext.OPENID
     getApp().globalData.token = result.res.data.access_token
     getApp().globalData.name = result.res.data.nickname
+
+    // 登录完成之后，刷新数据
+    this.getCloudNotes()
+    this.getDatas()
+    // 如果token有值，则显示更多功能
+    if (getApp().globalData.token) {
+      this.setData({
+        auth: true,
+      })
+    }
   },
 
-
+  /**
+   * 刷新数据
+   */
+  // 下拉页面时，刷新数据
+  onPullDownRefresh () {
+    this.getCloudNotes()
+    this.getDatas()
+  },
+  // 获取云笔记信息（云数据库）
+  getCloudNotes () {
+    if (!getApp().globalData.openId) {
+      return
+    }
+    // TODO
+  },
+  // 获取其他数据（Api）
+  getDatas () {
+    if (!getApp().globalData.token) {
+      return
+    }
+    // TODO
+  },
 })
